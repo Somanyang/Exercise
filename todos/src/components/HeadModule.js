fdd93dbc4e56a6d0927b28d9516102c8c6c82ce7
somanyang <913706610@qq.com>
2017-08-12 16:08:47 +0800

    081202

--- todos/src/components/HeadModule.js
+++ todos/src/components/HeadModule.js
@@ -73,9 +73,66 @@ class HeadModule extends Component{
 			arr:arr1
 		})
 	}
+	clearItem=()=>{
+		let {arr}=this.state;
+		let arr1=arr.filter((e,i)=>{
+			return !e.checked;
+		})
+		this.setState({
+			arr:arr1
+		})
+	}
+	chooseItem=(ev)=>{
+		let lis=Array.from(this.ul.children);
+		if(ev.target.tagName.toLowerCase()==='a'){
+			lis.forEach((e)=>{
+				e.children[0].className='';
+			})
+			ev.target.className='selected';
+			window.location=ev.target.href;
+			this.setState({});
+		}
+		
+	}
 	render(){
 		let {arr}=this.state;
-		let list=arr.map((e,i)=>{
+		/*if(arr.length===0&&localStorage.getItem('info')){
+			arr=JSON.parse(localStorage.getItem('info'));
+		}
+		if(arr.length){
+			localStorage.setItem('info',JSON.stringify(arr));
+		}*/
+		let str='';
+		if(window.location.hash){
+			str=window.location.hash.split('/')[1];
+		}else{
+			str='all';
+		}
+		let arr2=null;
+		let num=0;
+		switch(str){
+			case 'all':
+				arr2=Object.assign(arr);
+				num=arr2.filter(e=>{
+					return !!e.checked;
+				}).length;
+				break;
+			case 'active':
+				arr2=arr.filter(e=>{
+					return !e.checked;
+				});
+				num=arr2.length;
+				break;
+			case 'completed':
+				arr2=arr.filter(e=>{
+					return e.checked;
+				});
+				num=arr2.length;
+				break;
+			default:console.log(arr,num)
+				break;
+		}
+		let list=arr2.map((e,i)=>{
 			let data={
 				content:e.content,
 				checked:e.checked,
@@ -88,8 +145,8 @@ class HeadModule extends Component{
 			return <Items {...data} />
 		})
 		let onOff=false;
-		if(arr.length){
-			onOff=arr.every(e=>{
+		if(arr2.length){
+			onOff=arr2.every(e=>{
 				return e.checked
 			})
 		}
@@ -109,6 +166,26 @@ class HeadModule extends Component{
 	                <input className="toggle-all" type="checkbox" checked={allCheck} onChange={this.checkAll}/>
 	                <ul className="todo-list">{list}</ul>
 	            </section>
+	            <footer className="footer" >
+		          <span className="todo-count">
+		            <strong>{num}</strong>
+		            <span>条已选中</span>
+		          </span>
+		          <ul className="filters" onClick={this.chooseItem} ref={(ele)=>{this.ul=ele}}>
+		            <li>
+		              <a href="#/all" className="selected">全部</a>
+		            </li>
+		            <li>
+		              <a href="#/active">未完成</a>
+		            </li>
+		            <li>
+		              <a href="#/completed">已完成</a>
+		            </li>
+		          </ul>
+		          <button className="clear-completed" onClick={this.clearItem}>
+		              清除完成项
+		          </button>
+		        </footer>
             </section>
 		)
 	}
