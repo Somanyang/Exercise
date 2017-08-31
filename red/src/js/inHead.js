dbd58a0df43ebea47935770840354bc4fa340caa
somanyang <913706610@qq.com>
2017-08-31 16:22:50 +0800

    0831

--- red/src/js/inHead.js
+++
@@ -1,5 +0,0 @@
-var pixelradtio=1/window.devicePixelRatio;
-document.write('<meta name="viewport" content="width=device-width,initial-scale='+pixelradtio+',minimum-scale='+pixelradtio+',maximum-scale='+pixelradtio+',user-scalable=no"/>')
-var html=document.documentElement;
-var pageWidth=html.getBoundingClientRect().width;
-html.style.fontSize=pageWidth/16+'px';
